42fb955036fcfddc4f4bef545e0beb0272e618b8
Minjeong Kim <70756727+minjeong-kim-git@users.noreply.github.com>
2021-09-23 18:08:36 +0900

    Delete LandingPage.js

--- LandingPage.js
+++
@@ -1,6 +0,0 @@
-function menuToggle() {
-    const toggleMenu = document.querySelector('.toggleMenu');
-    const navigation = document.querySelector('.nav');
-    toggleMenu.classList.toggle('active');
-    navigation.classList.toggle('active');
-}
