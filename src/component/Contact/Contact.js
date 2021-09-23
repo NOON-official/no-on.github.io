3e22b48ba853d04e692e46c460ebed83d2f80e1f
minjeong <fran0666@naver.com>
2021-09-23 21:14:58 +0900

    Updatd Contact

---
+++ src/component/Contact/Contact.js
@@ -0,0 +1,30 @@
+import Logo from '../images/logo.png';
+import Symbol from '../images/symbol.svg';
+import './Contact.css';
+
+function Contact() {
+    return (
+        <div className="contact-container">
+            <div className="contact-image">
+                <img src={Symbol} className="contact-symbol"/>
+                <br/>
+                <img src={Logo} className="contact-logo"/>
+                <br/>
+                <p>contact@no-on.info</p>
+                <form className="contact-form">
+                    <div className="input-col-1">
+                        <input type="text" placeholder="Name *"/><br/>
+                        <input type="text" placeholder="E-mail *"/><br/>
+                        <input type="text" placeholder="Subject *"/>
+                    </div>
+                    <div className="input-col-2">
+                        <input type="text" placeholder="Message" className="input-message"/><br/>
+                        <button type="submit" className="submit-btn">SEND</button>
+                    </div>
+            </form>
+            </div>
+        </div>
+    )
+}
+
+export default Contact;
