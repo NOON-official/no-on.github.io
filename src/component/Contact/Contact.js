59a49534c5fdcddd6b5f20c90cd430d7c6463975
Moon Gyuwon <s201801402@hufs-gsuite.kr>
2021-12-03 14:56:32 +0900

    [REMOVE] remove contact form

--- src/component/Contact/Contact.js
+++ src/component/Contact/Contact.js
@@ -10,8 +10,8 @@ function Contact() {
                 <br/>
                 <img src={Logo} className="contact-logo"/>
                 <br/>
-                <p>contact@no-on.info</p>
-                <form className="contact-form">
+                <p id="email">contact@no-on.info</p>
+                {/* <form className="contact-form">
                     <div className="input-col-1">
                         <input type="text" placeholder="Name *"/><br/>
                         <input type="text" placeholder="E-mail *"/><br/>
@@ -21,7 +21,7 @@ function Contact() {
                         <input type="text" placeholder="Message" className="input-message"/><br/>
                         <button type="submit" className="submit-btn">SEND</button>
                     </div>
-            </form>
+                </form> */}
             </div>
         </div>
     )
